037d4ba18d60b2f2167389b3561d1ed9111feeb0
7isenko <7isenko.max@gmail.com>
2020-06-25 18:20:10 +0300

    +License file and some format

--- src/main/java/io/github/_7isenko/confusingminecraft/BlockPlaceListener.java
+++ src/main/java/io/github/_7isenko/confusingminecraft/BlockPlaceListener.java
@@ -13,27 +13,27 @@ import org.bukkit.event.block.BlockPlaceEvent;
 import java.lang.reflect.Field;
 
 public class BlockPlaceListener implements Listener {
-@EventHandler
+    @EventHandler
     public void onPlace(BlockPlaceEvent event) {
-    if (event.getBlockPlaced().getType().equals(Material.CAMPFIRE))
-        return;
-    Bukkit.getScheduler().runTaskLater(ConfusingMinecraft.plugin, () -> {
-        try {
-            if (event.getBlockPlaced().getType() == Material.AIR)
-                return;
-            Block block = CraftMagicNumbers.getBlock(event.getBlockPlaced().getType());
-            SoundEffectType set = block.getBlockData().r();
-            Field f = set.getClass().getDeclaredField("z");
-            f.setAccessible(true);
-            SoundEffect se = (SoundEffect) f.get(set);
-            Location location = event.getBlockPlaced().getLocation();
-            BlockPosition bp = new BlockPosition(location.getBlockX(), location.getBlockY(), location.getBlockZ());
-            ((CraftWorld)event.getPlayer().getWorld()).getHandle().playSound(null, bp, se, SoundCategory.BLOCKS, 1f, 1f);
+        if (event.getBlockPlaced().getType().equals(Material.CAMPFIRE))
+            return;
+        Bukkit.getScheduler().runTaskLater(ConfusingMinecraft.plugin, () -> {
+            try {
+                if (event.getBlockPlaced().getType() == Material.AIR)
+                    return;
+                Block block = CraftMagicNumbers.getBlock(event.getBlockPlaced().getType());
+                SoundEffectType set = block.getBlockData().r();
+                Field f = set.getClass().getDeclaredField("z");
+                f.setAccessible(true);
+                SoundEffect se = (SoundEffect) f.get(set);
+                Location location = event.getBlockPlaced().getLocation();
+                BlockPosition bp = new BlockPosition(location.getBlockX(), location.getBlockY(), location.getBlockZ());
+                ((CraftWorld) event.getPlayer().getWorld()).getHandle().playSound(null, bp, se, SoundCategory.BLOCKS, 1f, 1f);
 
-        } catch (Exception e) {
-            // ok fuck it
-        }
-        event.getBlockPlaced().breakNaturally();
-    }, 200);
-}
+            } catch (Exception e) {
+                // ok fuck it
+            }
+            event.getBlockPlaced().breakNaturally();
+        }, 200);
+    }
 }
